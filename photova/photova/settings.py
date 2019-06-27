ed6b30dd2825b2327cfcad3eb354d9e0c16b10fc
Tate <tateshep@gmail.com>
2019-06-27 22:39:50 +0000

    added environment variables

--- photova/photova/settings.py
+++ photova/photova/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jz1wgfa45$y@3n4dj!+8*42yw5(dlw@#(8&q7cssf1k$#l8v(a'
+SECRET_KEY = 'os.getenv("SECRET_KEY")'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- photova/photova/wsgi.py
+++ photova/photova/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'photova.settings')
+project_folder='~/home/tateshep/tateshep.pythonanywhere.com/photova'
+load_dotenv(os.path.join(project_folder, '.env'))
 
 application = get_wsgi_application()
